cb5f40b2b96e4b2f8780b7adbdc75c43e68aeed0
Braun Roland <rolibraun94@gmail.com>
2025-03-07 12:33:47 +0100

    EditBookData() kesz

--- book/Book.cs
+++ book/Book.cs
@@ -27,5 +27,24 @@ namespace book
         public string Author { get => author;}
         public int PageCount { get => pageCount;}
         public int ReleaseYear { get => releaseYear;}
+
+        public void EditBookData()
+        {
+            Console.Write("Add meg a Könyv nevét: ");
+            title = Console.ReadLine();
+            Console.Write("Add meg a Könyv szerzőjét: ");
+            author = Console.ReadLine();
+            Console.Write("Add meg a Könyv oldalszámát: ");
+            pageCount = int.Parse(Console.ReadLine());
+            Console.Write("Add meg a Könyv kiadási évét: ");
+            releaseYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sikeres módósítás!");
+            this.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{author}-{title}: |{releaseYear}|{pageCount} oldal";
+        }
     }
 }
